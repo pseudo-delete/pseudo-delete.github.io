ac7d8ce3e9962925165d0e985886dd5cfe33fbec
pseudo-delete <germiljohn@gmail.com>
2025-11-14 05:52:35 +0800

    corrected syntax of update

--- neuralnetwork/333_memorize_the_image/php/update_neural_data_row.php
+++ neuralnetwork/333_memorize_the_image/php/update_neural_data_row.php
@@ -9,7 +9,45 @@
     $weight = $_POST['weight'];
     $bias = $_POST['bias'];
 
-    $query = "INSERT INTO 333_memorize_the_image(id, input1, input2, input3, hidden1, hidden2, hidden3, output1, output2, output3, target1, target2, target3, weight1, weight2, weight3, weight4, weight5, weight6, weight7, weight8, weight9, weight10, weight11, weight12, weight13, weight14, weight15, weight16, weight17, weight18, bias1, bias2, bias3, bias4, bias5, bias6) VALUES (".$id.", ".$input[0].", ".$input[1].", ".$input[2].", ".$hidden[0].", ".$hidden[1].", ".$hidden[2].", ".$output[0].", ".$output[1].", ".$output[2].", ".$target[0].", ".$target[1].", ".$target[2].", ".$weight[0].", ".$weight[1].", ".$weight[2].", ".$weight[3].", ".$weight[4].", ".$weight[5].", ".$weight[6].", ".$weight[7].", ".$weight[8].", ".$weight[9].", ".$weight[10].", ".$weight[11].", ".$weight[12].", ".$weight[13].", ".$weight[14].", ".$weight[15].", ".$weight[16].", ".$weight[17].", ".$bias[0].", ".$bias[1].", ".$bias[2].", ".$bias[3].", ".$bias[4].", ".$bias[5].")";
+    $query = "UPDATE `333_memorize_the_image` SET 
+        input1=".$input[0].",
+        input2=".$input[1].",
+        input3=".$input[2].",
+        hidden1=".$hidden[0].",
+        hidden2=".$hidden[1].",
+        hidden3=".$hidden[2].",
+        output1=".$output[0].",
+        output2=".$output[1].",
+        output3=".$output[2].",
+        target1=".$target[0].",
+        target2=".$target[1].",
+        target3=".$target[2].",
+        weight1=".$weight[0].",
+        weight2=".$weight[1].",
+        weight3=".$weight[2].",
+        weight4=".$weight[3].",
+        weight5=".$weight[4].",
+        weight6=".$weight[5].",
+        weight7=".$weight[6].",
+        weight8=".$weight[7].",
+        weight9=".$weight[8].",
+        weight10=".$weight[9].",
+        weight11=".$weight[10].",
+        weight12=".$weight[11].",
+        weight13=".$weight[12].",
+        weight14=".$weight[13].",
+        weight15=".$weight[14].",
+        weight16=".$weight[15].",
+        weight17=".$weight[16].",
+        weight18=".$weight[17].",
+        bias1=".$bias[0].",
+        bias2=".$bias[1].",
+        bias3=".$bias[2].",
+        bias4=".$bias[3].",
+        bias5=".$bias[4].",
+        bias6=".$bias[5]."
+    WHERE id=".$id.";
+    ";
 
     // Execute the query using your database connection
     if ($conn->query($query) === TRUE) {
