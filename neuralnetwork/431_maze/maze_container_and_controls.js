952167ffa0ae43e5fe2a73eb43405ad4747364d4
pseudo-delete <germiljohn@gmail.com>
2025-11-23 17:07:25 +0800

    update on sizing of maze tiles and sizing

--- neuralnetwork/431_maze/maze_container_and_controls.js
+++ neuralnetwork/431_maze/maze_container_and_controls.js
@@ -9,10 +9,13 @@ $(function()
             for(let y=0;y<TileRowsCount;y++)
             {
                 // create tile at (x,y) iteration by row
+                $(".maze-container").css("width", (TileColumnsCount*width)+"px");
+                $(".maze-container").css("height", (TileRowsCount*height)+"px");
                 $(".maze-container").append("<div class='maze-tile' id='tile-"+x+"-"+y+"'></div>");
+
                 tileDiv[x][y] = $("#tile-"+x+"-"+y+"");
-                tileDiv[x][y].css("left", (x*32)+"px");
-                tileDiv[x][y].css("top", (y*32)+"px");
+                tileDiv[x][y].css("left", (x*50)+"px");
+                tileDiv[x][y].css("top", (y*50)+"px");
                 tileDiv[x][y].css("width", width+"px");
                 tileDiv[x][y].css("height", height+"px");
             }
