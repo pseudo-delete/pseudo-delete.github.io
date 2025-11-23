ec297e91c6a6b57a319f73b8f44f5af27f2e9715
pseudo-delete <germiljohn@gmail.com>
2025-11-23 16:26:36 +0800

    established a tile in maze container and made it's size auto so it will auto-size.

--- neuralnetwork/431_maze/maze_container_and_controls.js
+++ neuralnetwork/431_maze/maze_container_and_controls.js
@@ -1,7 +1,21 @@
 $(function()
 {
+    let tileDiv = [];
     function establishPlatform(TileColumnsCount, TileRowsCount)
     {
-        // set the css grid template
+        for(let y=0;y<TileRowsCount;y++)
+        {
+            tileDiv[x] = [];
+            for(let x=0;x<TileColumnsCount;x++)
+            {
+                // create tile at (x,y) iteration by row
+                tileDiv[x][y] = $("<div class='maze-tile' id='tile-"+x+"-"+y+"'></div>");
+                tileDiv.css("left", (x*32)+"px");
+                tileDiv.css("top", (y*32)+"px");
+                $(".maze-container").append(tileDiv);
+            }
+        }
     }
+
+    establishPlatform(10,10);
 });
