c13a8799f6e74b6c7ce66d898085c1c3b0ba65a9
pseudo-delete <germiljohn@gmail.com>
2025-11-23 17:37:07 +0800

    added allowance inside the top and bottom of the maze container for entry and exit of the maze itself

--- neuralnetwork/431_maze/maze_container_and_controls.js
+++ neuralnetwork/431_maze/maze_container_and_controls.js
@@ -6,18 +6,21 @@ $(function()
         for(let x=0;x<TileColumnsCount;x++)
         {
             tileDiv[x] = [];
-            for(let y=0;y<TileRowsCount;y++)
+            for(let y=0;y<=TileRowsCount+1;y++)// y starts at 1 to match tile id in order to put an allowance at the top for the exit area of the maze, also added +1 to the TileRowsCount for the entry area of the maze
             {
-                // create tile at (x,y) iteration by row
-                $(".maze-container").css("width", (TileColumnsCount*width)+"px");
-                $(".maze-container").css("height", (TileRowsCount*height)+"px");
-                $(".maze-container").append("<div class='maze-tile' id='tile-"+x+"-"+y+"'></div>");
+                if(y>0 && y < TileRowsCount+1)
+                {
+                    // create tile at (x,y) iteration by row
+                    $(".maze-container").css("width", (TileColumnsCount*width)+"px");
+                    $(".maze-container").css("height", (TileRowsCount*height)+"px");
+                    $(".maze-container").append("<div class='maze-tile' id='tile-"+x+"-"+y+"'></div>");
 
-                tileDiv[x][y] = $("#tile-"+x+"-"+y+"");
-                tileDiv[x][y].css("left", (x*50)+"px");
-                tileDiv[x][y].css("top", (y*50)+"px");
-                tileDiv[x][y].css("width", width+"px");
-                tileDiv[x][y].css("height", height+"px");
+                    tileDiv[x][y] = $("#tile-"+x+"-"+y+"");
+                    tileDiv[x][y].css("left", (x*50)+"px");
+                    tileDiv[x][y].css("top", (y*50)+"px");
+                    tileDiv[x][y].css("width", width+"px");
+                    tileDiv[x][y].css("height", height+"px");
+                }
             }
         }
     }
