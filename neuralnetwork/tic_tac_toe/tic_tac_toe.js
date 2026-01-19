b2e0f5527b7c2ea74afd0efda7577625257d05d1
pseudo-delete <germiljohn@gmail.com>
2026-01-20 04:56:18 +0800

    added a turn for O player and can now mark "O" in the tic tac toe grid

--- neuralnetwork/tic_tac_toe/tic_tac_toe.js
+++ neuralnetwork/tic_tac_toe/tic_tac_toe.js
@@ -1,6 +1,24 @@
 $(document).ready(function() {
+    // let board = [
+    //     ["", "", ""],
+    //     ["", "", ""],
+    //     ["", "", ""]
+    // ];
+    let currentPlayer = ["X", "O"]; // X always starts first
+    let currentPlayerIndex = 0;
+
     $(".cell").click(function() {
-        $(this).text("X");
+        if (currentPlayerIndex == 0)
+        {
+            $(this).text("X");
+            currentPlayerIndex = 1;
+        }
+        else if(currentPlayerIndex == 1)
+        {
+            $(this).text("O");
+            currentPlayerIndex = 0;
+        }
+
         // Add logic for computer's move and checking for win/draw
     });
 });
