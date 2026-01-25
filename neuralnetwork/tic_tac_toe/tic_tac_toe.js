4567100ef483106eb99a7e4cba67bbe47342e5a4
pseudo-delete <germiljohn@gmail.com>
2026-01-26 06:14:32 +0800

    fixing move counter display

--- neuralnetwork/tic_tac_toe/tic_tac_toe.js
+++ neuralnetwork/tic_tac_toe/tic_tac_toe.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
         if (currentPlayerIndex == 0)currentPlayerIndex = 1;
         else if(currentPlayerIndex == 1)currentPlayerIndex = 0;
 
-        $("#move-count").text(parseInt($(this).text()) + 1);
+        $("#move-count").text(parseInt($("#move-count").text()) + 1);
 
         // Add logic for computer's move and checking for win/draw
     });
