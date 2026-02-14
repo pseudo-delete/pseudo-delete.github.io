575fc02b7faee529becb7c6ea6a97f27bda8dd5c
pseudo-delete <germiljohn@gmail.com>
2026-02-15 07:06:03 +0800

    added battlefield and sound effect collection

---
+++ gero/games/gero_fight_arena/battlefield/battle.js
@@ -0,0 +1,11 @@
+$(function()
+{
+    $('body').css({
+        'background-image': 'url("../img/arena/L1_Local_Arena_inside.png")',
+        'background-size': 'cover',
+        'background-position': 'center',
+        'background-repeat': 'no-repeat',
+        'background-attachment': 'fixed'
+    });
+});
+
--- gero/games/gero_fight_arena/index.js
+++ gero/games/gero_fight_arena/index.js
@@ -6,10 +6,20 @@ setTimeout(() => {
     document.body.style.backgroundRepeat = 'no-repeat';
 }, 2500);// 25 seconds = 25000 milliseconds
 
+// function loadBattlefield() {
+//     document.body.style.backgroundImage = 'url("../img/arena/L1_Local_Arena_inside.png")';
+//     document.body.style.backgroundSize = 'cover';
+//     document.body.style.backgroundPosition = 'center';
+//     document.body.style.backgroundRepeat = 'no-repeat';
+// }
+
 $(function (){
     $('#button-play').on('click', function () {
         const sound = $('#play-sound')[0]; // get native audio element
         sound.currentTime = 0;
         sound.play();
+        window.open('battlefield/battle.html', '_self');
+        // $('body').load('battlefield/battle.html');
+        // loadBattlefield();
     });
 });
