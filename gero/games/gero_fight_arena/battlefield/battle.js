33c704a39d2e65ce60e289e81b298fc7024f8c55
pseudo-delete <germiljohn@gmail.com>
2026-02-16 04:38:25 +0800

    added skip function for intro instruction while loading game

--- gero/games/gero_fight_arena/battlefield/battle.js
+++ gero/games/gero_fight_arena/battlefield/battle.js
@@ -1,11 +1,21 @@
 $(function()
 {
-    $('body').css({
+    $('body').css(
+    {
         'background-image': 'url("../img/arena/L1_Local_Arena_inside.png")',
         'background-size': 'cover',
         'background-position': 'center',
         'background-repeat': 'no-repeat',
         'background-attachment': 'fixed'
+    });
+
+    $('#button-play').on('click', function() 
+    {
+        $('.intro-curtain').fadeOut(0, function()
+        {
+            $(this).remove();
+        });
+        $('.intro-curtain, .intro-curtain * ').css('z-index', '-1');
     });
 });
 
